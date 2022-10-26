e9e753bf929d546ee92fde9941ce3f3fb853b251
AhmedmohseHussein <115736284+AhmedmohseHussein@users.noreply.github.com>
2022-10-26 17:26:54 +0200

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -1,14 +1,9 @@
-import turtle
-
-    
+import turtle    
 t.color=("green")
 t.speed(0)
 t.width(5)
-def star():
-    
-      
+def star(): 
     for i in range(100):
       t.forward(150)
-      t.right(108)
-      
+      t.right(140)     
 star()
